a1eac548ff8c4d41eed8efabb3357d55c08d376a
thetalemon <sasami0314@gmail.com>
2023-03-22 22:35:17 +0900

    text: change description

--- constants/constants.ts
+++ constants/constants.ts
@@ -3,5 +3,5 @@ export const myUrl = `https://${myDomain}`
 export const myMainUrl = `https:/manasas.dev`
 export const myName = 'manasas'
 export const mySiteName = 'manas diary'
-export const mySiteDefaultDescription = 'まなさすの雑記。どうでもいいこと。'
+export const mySiteDefaultDescription = 'まなさすの雑記。とりとめもないこと。'
 export const myCopyLight = `© 2023 ${myName}`
