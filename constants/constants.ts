0890bd5a7a1a65c15ce80169d416dd694425d0aa
thetalemon <sasami0314@gmail.com>
2024-01-03 21:57:00 +0900

    URLミス修正

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,6 +1,6 @@
 export const myDomain = 'diary.manasas.dev'
 export const myUrl = `https://${myDomain}`
-export const myMainUrl = `https:/manasas.dev`
+export const myMainUrl = `https://manasas.dev`
 export const myName = 'manasas'
 export const mySiteName = 'manas diary'
 export const mySiteDefaultDescription = 'まなさすの雑記。とりとめもないこと。'
